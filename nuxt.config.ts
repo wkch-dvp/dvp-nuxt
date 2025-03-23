eafafc1bfa2cc17f5e7dc31eba3e1e611cd86cb9
wkch-dvp <warren@devastgoedplanners.nl>
2025-03-23 20:10:04 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
-import { defineNuxtConfig } from 'nuxt/config';
-import { resolve } from 'path';
-
 export default defineNuxtConfig({
-  // Extend the WooNuxt base theme
-  extends: [resolve(__dirname, './woonuxt_base')],
+  future: {
+    compatibilityVersion: 4,
+  },
 
-  // Ensure custom components are detected
-  components: [{ path: './app/components', pathPrefix: false }],
+  // Get all the pages, components, composables and plugins from the parent theme
+  extends: ['./woonuxt_base'],
 
-  // Nitro settings (adjust based on server capability)
+  components: [{ path: './components', pathPrefix: false }],
+
+  /**
+   * Depending on your servers capabilities, you may need to adjust the following settings.
+   * It will affect the build time but also increase the reliability of the build process.
+   * If you have a server with a lot of memory and CPU, you can remove the following settings.
+   * @property {number} concurrency - How many pages to prerender at once
+   * @property {number} interval - How long to wait between prerendering pages
+   * @property {boolean} failOnError - This stops the build from failing but the page will not be statically generated
+   */
   nitro: {
     prerender: {
       concurrency: 10,
@@ -17,10 +24,5 @@ export default defineNuxtConfig({
     },
   },
 
-  // Debugging: Log if Nuxt is loading the correct layers
-  hooks: {
-    'ready': (nuxt) => {
-      console.log('✅ Loaded layers:', nuxt.options._layers);
-    }
-  }
+  compatibilityDate: '2024-07-09',
 });
